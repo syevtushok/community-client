64b9ff1ba08f8d4cc08f0478afe4b129554e8b09
Serhii Yevtushok <ggazila@gmail.com>
2025-01-09 19:30:12 +0200

    improvements

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://api.algorithm-challenge.com';
 export const TOKEN_KEY = 'token';
 export const AUTH_ROUTES = {
     LOGIN: '/login'
